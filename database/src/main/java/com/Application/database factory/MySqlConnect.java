e2676f3ed833f76642745e5a9d118b9b3fee21a1
Aaditya <aadityakam@gmail.com>
2025-07-08 17:12:18 +0530

    Create MySqlConnect.java

---
+++ database/src/main/java/com/Application/database factory/MySqlConnect.java
@@ -0,0 +1,8 @@
+package com.medlife.databasefactory;
+
+import java.util.logging.Logger;
+
+public class MySqlConnect {
+
+    public Logger logger = Logger.getLogger(this.getClass().getSimpleName());
+}
